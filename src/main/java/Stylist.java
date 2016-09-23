1404f59fc20df46dc18462a177ccc3adeb634498
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-09-23 08:08:51 -0700

    initial commit

---
+++ src/main/java/Stylist.java
@@ -0,0 +1,8 @@
+import java.util.List;
+import java.util.ArrayList;
+import org.sql2o.*;
+
+public class Stylist {
+
+
+}
---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,14 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+
+
+public class ClientTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+
+
+}
